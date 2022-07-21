e150fcd59060d1012f19558eaadfa9199ca22f52
zjouahri <zakaria.jouahri@capgemini.com>
2022-07-21 13:20:58 +0200

    Singleton Java Config

--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -1,5 +1,7 @@
+import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 import ziko.spring.core.repository.HibernateSpeakerRepositoryImpl;
 import ziko.spring.core.repository.SpeakerRepository;
 import ziko.spring.core.service.SpeakerService;
@@ -20,6 +22,7 @@ public class AppConfig {
 
 // Construction Injection
     @Bean(name = "speakerService")
+    @Scope(value = BeanDefinition.SCOPE_SINGLETON)
     public SpeakerService getSpeakerService() {
         SpeakerServiceImpl service = new SpeakerServiceImpl(getSpeakerRepository());
         return service;
--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -12,6 +12,11 @@ public class Application {
 
         SpeakerService service = applicationContext.getBean("speakerService", SpeakerService.class);
 
+        System.out.println(service);
+
+        SpeakerService service2 = applicationContext.getBean("speakerService", SpeakerService.class);
+        System.out.println(service2);
+
         System.out.println(service.findAll().get(0).getFirstName());
     }
 }
