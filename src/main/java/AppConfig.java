fadf3acc6b4675aa4271c9d1bf95b12f35b4d846
zjouahri <zakaria.jouahri@capgemini.com>
2022-07-21 17:08:53 +0200

    using FactoryBean

--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -1,9 +1,25 @@
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import ziko.spring.core.util.CalendarFactory;
+
+import java.util.Calendar;
 
 @Configuration
 @ComponentScan({"ziko.spring.core"})
 public class AppConfig {
+
+    @Bean(name = "cal")
+    public CalendarFactory calendarFactory () {
+        CalendarFactory factory = new CalendarFactory();
+        factory.addDays(2);
+        return factory;
+    }
+
+    @Bean
+    public Calendar calendar() throws Exception {
+        return calendarFactory().getObject();
+    }
 
 /**
  * Setter Injection
--- src/main/java/ziko/spring/core/repository/HibernateSpeakerRepositoryImpl.java
+++ src/main/java/ziko/spring/core/repository/HibernateSpeakerRepositoryImpl.java
@@ -1,13 +1,18 @@
 package ziko.spring.core.repository;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import ziko.spring.core.model.Speaker;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 @Repository("SpeakerRepository")
 public class HibernateSpeakerRepositoryImpl implements SpeakerRepository {
+
+    @Autowired
+    private Calendar calendar;
 
     @Override
     public List<Speaker> findAll() {
@@ -18,6 +23,7 @@ public class HibernateSpeakerRepositoryImpl implements SpeakerRepository {
         speaker.setFirstName("Zakaria");
         speaker.setLastName("Jouahri");
 
+        System.out.println("calendar: " + calendar.getTime());
         speakers.add(speaker);
 
         return speakers;
