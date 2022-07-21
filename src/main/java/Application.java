f30f3ede27ff4902088ed780ca3268b241c6fc2b
zjouahri <zakaria.jouahri@capgemini.com>
2022-07-21 14:30:53 +0200

    Setter Injection with XML

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,10 +1,16 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import ziko.spring.core.service.SpeakerService;
 import ziko.spring.core.service.SpeakerServiceImpl;
 
 public class Application {
 
     public static void main(String[] args) {
-        SpeakerService service = new SpeakerServiceImpl();
+
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext( "applicationContext.xml");
+
+        //SpeakerService service = new SpeakerServiceImpl();
+        SpeakerService service = applicationContext.getBean("speakerService", SpeakerService.class);
 
         System.out.println(service.findAll().get(0).getFirstName());
     }
--- src/main/java/ziko/spring/core/service/SpeakerServiceImpl.java
+++ src/main/java/ziko/spring/core/service/SpeakerServiceImpl.java
@@ -1,17 +1,20 @@
 package ziko.spring.core.service;
 
 import ziko.spring.core.model.Speaker;
-import ziko.spring.core.repository.HibernateSpeakerRepositoryImpl;
 import ziko.spring.core.repository.SpeakerRepository;
 
 import java.util.List;
 
 public class SpeakerServiceImpl implements SpeakerService {
 
-    private SpeakerRepository repository = new HibernateSpeakerRepositoryImpl();
+    private SpeakerRepository repository;
 
     @Override
     public List<Speaker> findAll() {
         return repository.findAll();
+    }
+
+    public void setSpeakerRepository(SpeakerRepository repository) {
+        this.repository = repository;
     }
 }
